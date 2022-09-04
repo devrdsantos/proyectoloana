2352dcfa7a2c63fbbb35589afd4fd4b4d500bc99
daniel <santoss.rdaniel@gmail.com>
2022-09-03 21:05:57 -0300

    [ADD] header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import CarouselMain from "./components/CarouselMain";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <CarouselMain />
     </div>
   );
