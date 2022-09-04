0742eac5eb7fdb79de3079a48594511788660146
Patricio <patoog58@gmail.com>
2022-09-03 21:00:26 -0300

    [ADD] añado  footer + añado rama Patricio

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import CarouselMain from "./components/CarouselMain";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <CarouselMain />
+    <Footer />
     </div>
+
   );
 }
 
