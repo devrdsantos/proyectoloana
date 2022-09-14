1ed0883e7e6be7c505cc099b6188ef8e48411f58
Patricio <patoog58@gmail.com>
2022-09-13 23:20:21 -0300

    [add]merge daniel

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,96 +7,95 @@ import { Link } from "react-router-dom";
 import "../styles/footer.css";
 
 function Footer() {
-    return (
+  return (
     <footer>
       {/* FOOTER DESKTOP */}
-        <div className="container-fluid footer-desktop">
+      <div className="container-fluid footer-desktop">
         <div className="row p-5 pb-2 bg-black text-white">
-            <div className="col-xs-12 col-md-6 col-lg-3">
+          <div className="col-xs-12 col-md-6 col-lg-3">
             <div className="h3">
-                <img src={logoFooter} width="200" alt="logo negro" />
+              <img src={logoFooter} width="200" alt="logo negro" />
             </div>
+          </div>
 
-            <div className="col-xs-12 col-md-6 col-lg-3">
-                <p className="h5 mb-2 text-white bbt-1">Productos</p>
-                <div className="mb-2">
-                <button className="button-decoration-footer text-secondary text-decoration-none">
-                    <Link className=" link-footer-color" to="/jeans">
-                    Jeans
-                    </Link>
-                </button>
-                </div>
-                <div className="mb-2">
-                <button className=" button-decoration-footer text-secondary text-decoration-none">
-                    <Link className=" link-footer-color" to="/jeans">
-                    Shorts
-                    </Link>
-                </button>
-                </div>
-                <div className="mb-2">
-                <button className=" button-decoration-footer text-secondary text-decoration-none">
-                    <Link className=" link-footer-color" to="/jeans">
-                    Camperas
-                    </Link>
-                </button>
-                </div>
-                <div className="mb-2">
-                <button className=" button-decoration-footer text-secondary text-decoration-none">
-                    <Link className=" link-footer-color" to="/jeans">
-                    Ofertas
-                    </Link>
-                </button>
-                </div>
+          <div className="col-xs-12 col-md-6 col-lg-3">
+            <p className="h5 mb-2 text-white">Productos</p>
+            <div className="mb-2">
+              <button className="button-decoration-footer text-secondary text-decoration-none">
+                <Link className=" link-footer-color" to="/jeans">
+                  Jeans
+                </Link>
+              </button>
             </div>
+            <div className="mb-2">
+              <button className=" button-decoration-footer text-secondary text-decoration-none">
+                <Link className=" link-footer-color" to="/jeans">
+                  Shorts
+                </Link>
+              </button>
+            </div>
+            <div className="mb-2">
+              <button className=" button-decoration-footer text-secondary text-decoration-none">
+                <Link className=" link-footer-color" to="/jeans">
+                  Camperas
+                </Link>
+              </button>
+            </div>
+            <div className="mb-2">
+              <button className=" button-decoration-footer text-secondary text-decoration-none">
+                <Link className=" link-footer-color" to="/jeans">
+                  Ofertas
+                </Link>
+              </button>
+            </div>
+          </div>
 
-            <div className="col-xs-12 col-md-6 col-lg-3">
-                <p className="h5 mb-2 text-white">Contacto</p>
-                <div className="d-flex mb-2 text-secondary">
-                {" "}
-                <img
-                    className="font-size-footer-logo-direction-number"
-                    src={iconosFooter}
-                    alt="ubicacion"
-                />{" "}
-                <p className=" link-footer-color">Av. San Martín 1587, CABA</p>
-                </div>
-                <div className="d-flex mb-2 text-secondary">
-                {" "}
-                <img
-                    className="font-size-footer-logo-direction-number"
-                    src={iconosFooter2}
-                    alt="whatsapp"
-                />{" "}
-                <p className=" link-footer-color">(+54) 9 11-5674-1599</p>
-                </div>
+          <div className="col-xs-12 col-md-6 col-lg-3">
+            <p className="h5 mb-2 text-white">Contacto</p>
+            <div className="d-flex mb-2 text-secondary">
+              {" "}
+              <img
+                className="font-size-footer-logo-direction-number"
+                src={iconosFooter}
+                alt="ubicacion"
+              />{" "}
+              <p className=" link-footer-color">Av. San Martín 1587, CABA</p>
             </div>
+            <div className="d-flex mb-2 text-secondary">
+              {" "}
+              <img
+                className="font-size-footer-logo-direction-number"
+                src={iconosFooter2}
+                alt="whatsapp"
+              />{" "}
+              <p className=" link-footer-color">(+54) 9 11-5674-1599</p>
+            </div>
+          </div>
 
-            <div className="col-xs-12 col-md-6 col-lg-3">
-                <p className="h5 text-white">Nuestras Redes</p>
-                <div className="mb-2">
-                <button className=" button-decoration-footer text-secondary text-decoration-none">
-                    <Link className=" link-footer-color" to="/jeans">
-                    Facebook
-                    </Link>
-                </button>
-                </div>
-                <div className="mb-2"></div>
-                <div className="mb-2">
-                <button className=" button-decoration-footer text-secondary text-decoration-none">
-                    <Link className=" link-footer-color" to="/jeans">
-                    Instagram
-                    </Link>
-                </button>
-                </div>
-                <div className="mb-2"></div>
+          <div className="col-xs-12 col-md-6 col-lg-3">
+            <p className="h5 text-white">Nuestras Redes</p>
+            <div className="mb-2">
+              <button className=" button-decoration-footer text-secondary text-decoration-none">
+                <Link className=" link-footer-color" to="/jeans">
+                  Facebook
+                </Link>
+              </button>
             </div>
-            <div className="text-center pt-3 col-xs-12">
-                © Loana Jeans 2020 - 2022 . Todos los derechos reservados.
+            <div className="mb-2"></div>
+            <div className="mb-2">
+              <button className=" button-decoration-footer text-secondary text-decoration-none">
+                <Link className=" link-footer-color" to="/jeans">
+                  Instagram
+                </Link>
+              </button>
             </div>
+            <div className="mb-2"></div>
+          </div>
+          <div className="text-center pt-3 col-xs-12">
+            © Loana Jeans 2020 - 2022 . Todos los derechos reservados.
+          </div>
         </div>
       </div>
-      </div>
-
       {/* FOOTER MOBILE */}
       <div className="container-fluid bg-black footer-mobile">
         <div className="d-flex justify-content-between">
@@ -126,9 +125,8 @@ function Footer() {
           © Loana Jeans 2020 - 2022 . Todos los derechos reservados.
         </div>
       </div>
-
     </footer>
-    );
+  );
 }
 
 export default Footer;
