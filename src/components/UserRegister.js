8616546942cb5832433594fbe2786bbbb2f7b7ec
Franco <francoherrera77003@gmail.com>
2022-09-16 19:07:05 -0300

    [ADD] cambios en los botones de google y facebook

--- src/components/UserRegister.js
+++ src/components/UserRegister.js
@@ -2,6 +2,7 @@ import React from "react";
 import loana1 from "../assets/loana1.png"
 import google from "../assets/google.png"
 import facebook from "../assets/facebook.png"
+import "../styles/userregister.css"
 
 function UserRegister() {
     return (
@@ -15,22 +16,23 @@ function UserRegister() {
                     <button className="btn-size-register">REGISTRARSE</button> <span className="linea-central-botones">|</span>
                     <button className="btn-size-register">INICIAR SESIÓN</button>
                 </div>
-            </div>
-            <div className="contenedor-seccion-principal">
+                <div className="contenedor-seccion-principal">
                 <form action="" className="form-registro" method="post">
-                    <label for="email">DIRECCIÓN DE E-MAIL:</label>
+                    <label className="label-mail-contrasenia" for="email">DIRECCIÓN DE E-MAIL:</label>
                     <input type="email" name="email" id="email" autocomplete="off"/>
-                    <label for="contrasenia">CONTRASEÑA:</label>
+                    <label for="contrasenia" className="label-mail-contrasenia">CONTRASEÑA:</label>
                     <input type="password" name="contrasenia" id="contrasenia" autocomplete="off"/>
                     <p href="#" class="link-cambio-contrasenia">¿Has olvidado tu contraseña?</p>
                     <input type="submit" value="INGRESAR" class="btn-ingresar"/>
                 </form>
-                <p>TAMBIÉN PUEDES INICIAR SESIÓN CON</p>
+                <p className="parrafo-inicio-sesion-redes">TAMBIÉN PUEDES INICIAR SESIÓN CON</p>
                 <div class="contenedor-google-facebook">
-                    <span class="btn-ingreso-redes-sociales"> <img src={google} alt=""/><p href="">Google</p></span>
-                    <span class="btn-ingreso-redes-sociales"> <img src={facebook} alt=""/><p href="">Facebook</p> </span>
+                    <span class="btn-ingreso-redes-sociales"> <img src={google} alt=""/><p href="">GOOGLE</p></span>
+                    <span class="btn-ingreso-redes-sociales"> <img src={facebook} alt=""/><p href="">FACEBOOK</p> </span>
                 </div>
             </div>
+            </div>
+
         </div>
         </div>
     )
