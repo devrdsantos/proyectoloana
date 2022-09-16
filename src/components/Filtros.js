c735321597ff554ba7fb9bfb9785a4df6be10213
marco <massantemarcolucas@gmail.com>
2022-09-16 05:15:49 -0300

    [ADD FILTROS]

---
+++ src/components/Filtros.js
@@ -0,0 +1,31 @@
+import React, {useState} from "react";
+import "../styles/filtros.css";
+
+
+function Filtros({ordenar}) {
+    const handleChange = (e) =>{
+        ordenar(e)
+    };
+
+    return(
+        <div className="principal_countainer">
+        <div className="filtrar_por">
+            <span> Filtrar por: </span>
+        </div>
+        <div className="secondary_countainer">
+            <select
+                className= "menu"
+                defaultValue="relevante"
+                ame="select"
+                onChange={handleChange}
+            >
+                    <option value="relevante">Mas relevante</option>
+                    <option value="descendente">Mayor precio</option>
+                    <option value="ascendente">Menor precio</option>
+            </select>
+        </div>
+    </div>
+
+    )};
+
+export default Filtros;
