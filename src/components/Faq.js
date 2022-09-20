e32026f4648b2e9e487fee948cb46586f3a792f7
Patricio <patoog58@gmail.com>
2022-09-20 15:44:43 -0300

    [add] modifico Faq.js y faq.css

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -3,102 +3,103 @@ import "../styles/faq.css"
 
 function Faq() {
     return (
-    <div className="main">
-        <h1 className="main_h1">F.A.Q</h1>
-        <ul className="main_ul">
-          <li className="main-ul_li">
-            <input type="checkbox" checked />
-            <i></i>
-            <h2 className="main-h2">Comprar en LoanaJeans.com.ar</h2>
-            <p className="main-ul-li_p" >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem ex provident in nisi officiis eius optio blanditiis tempore temporibus, voluptas cum, suscipit labore et, fugiat esse? Ad eveniet animi repudiandae.lorem Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error aperiam mollitia repellat cupiditate iste commodi accusantium eligendi. Cumque hic a aliquam! Nobis sunt et est corporis deserunt odio facere consectetur?</p>
-          </li>
-          <li className="main-ul_li">
-            <input type="checkbox" checked />
-            <i></i>
-            <h2 className="main-h2">Envios y Estado del pedido</h2>
-            <p className="main-ul-li_p" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, neque? Quas, aspernatur necessitatibus. Fugit eius reprehenderit reiciendis sapiente impedit aut, ipsa explicabo ipsum quam, delectus, fuga eaque unde consequuntur. Reiciendis.
-            Quia soluta laudantium voluptate nobis ratione, sequi dicta doloribus ex dolorum quas! At quod sunt, repellendus eius culpa sapiente ipsam rem quaerat laborum, rerum incidunt architecto quis laboriosam nulla minima.</p>
-          </li>
-    
-          <li className="main-ul_li">
-            <input type="checkbox" checked />
-            <i></i>
-            <h2 className="main-h2">Cambios y Devoluciones</h2>
-            <p className="main-ul-li_p" >
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio maiores natus sint. Corporis et sed eaque quisquam vitae a perferendis non nostrum hic neque? Obcaecati porro ad libero distinctio sapiente.
-              Alias tempora delectus repellendus quis ut iure voluptatum, accusantium excepturi odit facilis temporibus mollitia! Excepturi, iste dolore itaque error deleniti at rem voluptate. Quae sit quas nihil eum veniam. Aspernatur.
-            </p>
-          </li>
-    
-          <li className="main-ul_li">
-            <input type="checkbox" checked />
-            <i></i>
-            <h2 className="main-h2">Pagos</h2>
-            <p className="main-ul-li_p" >
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio maiores natus sint. Corporis et sed eaque quisquam vitae a perferendis non nostrum hic neque? Obcaecati porro ad libero distinctio sapiente.
-              Alias tempora delectus repellendus quis ut iure voluptatum, accusantium excepturi odit facilis temporibus mollitia! Excepturi, iste dolore itaque error deleniti at rem voluptate. Quae sit quas nihil eum veniam. Aspernatur.
-            </p>
-          </li>
-    
-          <li className="main-ul_li">
-            <input type="checkbox" checked />
-            <i></i>
-            <h2 className="main-h2">Productos</h2>
-            <p className="main-ul-li_p">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, alias officia sapiente minima tenetur non doloremque veniam dolore error quo mollitia sunt officiis cum cupiditate, amet pariatur harum sit omnis.
-            Iure, distinctio incidunt dignissimos aspernatur cumque minima architecto quis, obcaecati officiis adipisci rem, praesentium vel dicta eum itaque sequi repellat ipsam ab assumenda! Sit, fugit. Saepe magni accusantium quisquam illum!
-            </p>
-          </li>
-    
-          <li className="main-ul_li">
-            <input type="checkbox" checked />
-            <i></i>
-            <h2  className="main-h2">Targeta de regalos</h2>
-            <p className="main-ul-li_p">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, alias officia sapiente minima tenetur non doloremque veniam dolore error quo mollitia sunt officiis cum cupiditate, amet pariatur harum sit omnis.
-            Iure, distinctio incidunt dignissimos aspernatur cumque minima architecto quis, obcaecati officiis adipisci rem, praesentium vel dicta eum itaque sequi repellat ipsam ab assumenda! Sit, fugit. Saepe magni accusantium quisquam illum!
-            </p>
-          </li>
-    
-          <li class="main-ul_li">
-            <input type="checkbox" checked />
-            <i></i>
-            <h2 class="main-h2">Tienda y Empresa</h2>
-            <p class="main-ul-li_p">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam aspernatur at eaque ullam nulla laborum ab voluptas accusamus necessitatibus eos reiciendis facere repellendus iste consequuntur, placeat laboriosam temporibus ea! Ex.
-              Pariatur temporibus debitis placeat vitae perspiciatis amet! Id, suscipit error veniam natus repellat saepe perspiciatis et? Corporis velit, dolore a veniam nisi dolores exercitationem ipsam saepe, ea expedita, odio est.
-            </p>
-          </li>
-    
-          <li class="main-ul_li">
-            <input type="checkbox" checked />
-            <i></i>
-            <h2 class="main-h2">Programa Recogida de ropa</h2>
-            <p class="main-ul-li_p">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam fugit vero provident earum facilis. Libero quas magnam quaerat voluptates maxime similique dolores illo atque voluptatum inventore, molestias sit, perferendis ex.
-              Atque dolorem consequatur quam, quis doloremque tempora magni omnis perspiciatis ut dolores libero id obcaecati quia itaque nesciunt mollitia suscipit illo? Quae quas quia quam laboriosam corrupti nam debitis quasi.
-            </p>
-          </li>
-    
-          <li class="main-ul_li">
-            <input type="checkbox" checked />
-            <i></i>
-            <h2 class="main-h2">Compra Segura</h2>
-            <p class="main-ul-li_p">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente non ex sint provident deserunt quis nisi! Ullam, amet! Possimus repellendus ex aperiam error dolores quaerat totam accusantium ut consequatur officia! Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, vero hic cum nesciunt delectus magnam quisquam perspiciatis alias, beatae ab error omnis commodi totam voluptate id debitis mollitia neque perferendis!
-            </p>
-          </li>
-    
-          <li class="main-ul_li">
-            <input type="checkbox" checked />
-            <i></i>
-            <h2 class="main-h2">Como ponerte en Contacto con Nuestras Tiendas</h2>
-            <p class="main-ul-li_p">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente non ex sint provident deserunt quis nisi! Ullam, amet! Possimus repellendus ex aperiam error dolores quaerat totam accusantium ut consequatur officia! Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, vero hic cum nesciunt delectus magnam quisquam perspiciatis alias, beatae ab error omnis commodi totam voluptate id debitis mollitia neque perferendis!
-            </p>
-          </li>
-        </ul>
-    </div>
+      <div class="main">
+    {/*<h1>Preguntas Frecuentes</h1>*/}
+    <img src="https://img.icons8.com/sf-ultralight/100/000000/faq.png"/>
+    <ul class="main_ul">
+      <li class="main-ul_li">
+        <input type="checkbox" checked />
+        <img src="https://img.icons8.com/carbon-copy/50/000000/shopping-bag.png"/>
+        <h2 class="main-h2">Comprar en LoanaJeans.com.ar</h2>
+        <p class="main-ul-li_p" >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem ex provident in nisi officiis eius optio blanditiis tempore temporibus, voluptas cum, suscipit labore et, fugiat esse? Ad eveniet animi repudiandae.lorem Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error aperiam mollitia repellat cupiditate iste commodi accusantium eligendi. Cumque hic a aliquam! Nobis sunt et est corporis deserunt odio facere consectetur?</p>
+      </li>
+      <li class="main-ul_li">
+        <input type="checkbox" checked />
+        <img src="https://img.icons8.com/sf-ultralight/50/000000/free-shipping.png"/>
+        <h2 class="main-h2">Envios y Estado del pedido</h2>
+        <p class="main-ul-li_p" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, neque? Quas, aspernatur necessitatibus. Fugit eius reprehenderit reiciendis sapiente impedit aut, ipsa explicabo ipsum quam, delectus, fuga eaque unde consequuntur. Reiciendis.
+        Quia soluta laudantium voluptate nobis ratione, sequi dicta doloribus ex dolorum quas! At quod sunt, repellendus eius culpa sapiente ipsam rem quaerat laborum, rerum incidunt architecto quis laboriosam nulla minima.</p>
+      </li>
+
+      <li class="main-ul_li">
+        <input type="checkbox" checked />
+        <img src="https://img.icons8.com/sf-ultralight/50/000000/available-updates.png"/>
+        <h2 class="main-h2">Cambios y Devoluciones</h2>
+        <p class="main-ul-li_p" >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio maiores natus sint. Corporis et sed eaque quisquam vitae a perferendis non nostrum hic neque? Obcaecati porro ad libero distinctio sapiente.
+          Alias tempora delectus repellendus quis ut iure voluptatum, accusantium excepturi odit facilis temporibus mollitia! Excepturi, iste dolore itaque error deleniti at rem voluptate. Quae sit quas nihil eum veniam. Aspernatur.
+        </p>
+      </li>
+
+      <li class="main-ul_li">
+        <input type="checkbox" checked />
+        <img src="https://img.icons8.com/carbon-copy/50/000000/online-money-transfer.png"/>
+        <h2 class="main-h2">Pagos</h2>
+        <p class="main-ul-li_p" >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio maiores natus sint. Corporis et sed eaque quisquam vitae a perferendis non nostrum hic neque? Obcaecati porro ad libero distinctio sapiente.
+          Alias tempora delectus repellendus quis ut iure voluptatum, accusantium excepturi odit facilis temporibus mollitia! Excepturi, iste dolore itaque error deleniti at rem voluptate. Quae sit quas nihil eum veniam. Aspernatur.
+        </p>
+      </li>
+
+      <li class="main-ul_li">
+        <input type="checkbox" checked />
+        <img src="https://img.icons8.com/sf-ultralight/50/000000/trousers.png"/>
+        <h2 class="main-h2">Productos</h2>
+        <p class="main-ul-li_p">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, alias officia sapiente minima tenetur non doloremque veniam dolore error quo mollitia sunt officiis cum cupiditate, amet pariatur harum sit omnis.
+        Iure, distinctio incidunt dignissimos aspernatur cumque minima architecto quis, obcaecati officiis adipisci rem, praesentium vel dicta eum itaque sequi repellat ipsam ab assumenda! Sit, fugit. Saepe magni accusantium quisquam illum!
+        </p>
+      </li>
+
+      <li class="main-ul_li">
+        <input type="checkbox" checked />
+        <img src="https://img.icons8.com/carbon-copy/50/000000/gift-card.png"/>
+        <h2  class="main-h2">Targeta de regalos</h2>
+        <p class="main-ul-li_p">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, alias officia sapiente minima tenetur non doloremque veniam dolore error quo mollitia sunt officiis cum cupiditate, amet pariatur harum sit omnis.
+        Iure, distinctio incidunt dignissimos aspernatur cumque minima architecto quis, obcaecati officiis adipisci rem, praesentium vel dicta eum itaque sequi repellat ipsam ab assumenda! Sit, fugit. Saepe magni accusantium quisquam illum!
+        </p>
+      </li>
+
+      <li class="main-ul_li">
+        <input type="checkbox" checked />
+        <img src="https://img.icons8.com/carbon-copy/50/000000/organization.png"/>
+        <h2 class="main-h2">Tienda y Empresa</h2>
+        <p class="main-ul-li_p">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam aspernatur at eaque ullam nulla laborum ab voluptas accusamus necessitatibus eos reiciendis facere repellendus iste consequuntur, placeat laboriosam temporibus ea! Ex.
+          Pariatur temporibus debitis placeat vitae perspiciatis amet! Id, suscipit error veniam natus repellat saepe perspiciatis et? Corporis velit, dolore a veniam nisi dolores exercitationem ipsam saepe, ea expedita, odio est.
+        </p>
+      </li>
+
+      <li class="main-ul_li">
+        <input type="checkbox" checked />
+        <img src="https://img.icons8.com/sf-ultralight/50/000000/clothes.png"/>
+        <h2 class="main-h2">Programa Recogida de ropa</h2>
+        <p class="main-ul-li_p">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam fugit vero provident earum facilis. Libero quas magnam quaerat voluptates maxime similique dolores illo atque voluptatum inventore, molestias sit, perferendis ex.
+          Atque dolorem consequatur quam, quis doloremque tempora magni omnis perspiciatis ut dolores libero id obcaecati quia itaque nesciunt mollitia suscipit illo? Quae quas quia quam laboriosam corrupti nam debitis quasi.
+        </p>
+      </li>
+
+      <li class="main-ul_li">
+        <input type="checkbox" checked />
+        <img src="https://img.icons8.com/sf-ultralight/50/000000/paid.png"/>
+        <h2 class="main-h2">Compra Segura</h2>
+        <p class="main-ul-li_p">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente non ex sint provident deserunt quis nisi! Ullam, amet! Possimus repellendus ex aperiam error dolores quaerat totam accusantium ut consequatur officia! Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, vero hic cum nesciunt delectus magnam quisquam perspiciatis alias, beatae ab error omnis commodi totam voluptate id debitis mollitia neque perferendis!
+        </p>
+      </li>
+
+      <li class="main-ul_li">
+        <input type="checkbox" checked />
+        <img src="https://img.icons8.com/dotty/50/000000/about-us-female.png"/>
+        <h2 class="main-h2">Como ponerte en Contacto con Nuestras Tiendas</h2>
+        <p class="main-ul-li_p">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente non ex sint provident deserunt quis nisi! Ullam, amet! Possimus repellendus ex aperiam error dolores quaerat totam accusantium ut consequatur officia! Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, vero hic cum nesciunt delectus magnam quisquam perspiciatis alias, beatae ab error omnis commodi totam voluptate id debitis mollitia neque perferendis!
+        </p>
+      </li>
+    </ul>
+  </div>
     );
   }
   
